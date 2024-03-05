c8d3e9f5d07429ffafde4d45279180ee0c744834
万雅虎 <vipwan@outlook.com>
2024-03-05 14:16:02 +0800

    Update StoreDbContext.cs

--- Biwen.Settings.MutiNodeTest.Shared/StoreDbContext.cs
+++ Biwen.Settings.MutiNodeTest.Shared/StoreDbContext.cs
@@ -9,12 +9,8 @@ namespace BiwenSettingsMutiNodeTest.Shared
     /// <summary>
     /// 公用一个数据库的情况下,可以使用同一个DbContext
     /// </summary>
-    public class StoreDbContext : DbContext, IBiwenSettingsDbContext
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options), IBiwenSettingsDbContext
     {
-        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Setting> Settings { get; set; }
     }
 }
