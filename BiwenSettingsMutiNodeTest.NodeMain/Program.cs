fcd7a36c7fc88458491b50777d1f192287bf5c62
万雅虎 <vipwan@outlook.com>
2024-05-22 15:24:55 +0800

    nuget

--- BiwenSettingsMutiNodeTest.NodeMain/Program.cs
+++ BiwenSettingsMutiNodeTest.NodeMain/Program.cs
@@ -1,7 +1,9 @@
 ﻿using BiwenSettingsMutiNodeTest.Shared.Settings;
 using Biwen.Settings;
+using Biwen.Settings.Extensions.Configuration;
 using BiwenSettingsMutiNodeTest.Shared;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,24 +31,35 @@ builder.Services.AddBiwenSettings(o =>
     });
 
     //主节点开启通知
-    o.NotifyOption.IsNotifyEnable = true;
-    o.NotifyOption.EndpointHosts =
+    o.NotifyOptions.IsNotifyEnable = true;
+    o.NotifyOptions.EndpointHosts =
     [
         "http://localhost:5041",
         "http://localhost:5042" //消费节点
     ];
 });
 
+//扩展支持到IConfiguration & IOptions
+builder.Configuration.AddBiwenSettingConfiguration(builder.Services, true);
 
 var app = builder.Build();
 
 //主要节点无需配置通知路由
 app.UseBiwenSettings("biwen-settings/api", false);
 
-//拉取配置
-app.MapGet("/", (GitHubSetting setting) =>
+//拉取配置,可以直接注入T或者使用IOptions<T>
+app.MapGet("/", (
+    IOptions<GitHubSetting> setting, //启动后不会变更
+    IOptionsSnapshot<GitHubSetting> snapshot, //变更后同步变更
+    GitHubSetting setting2 //变更后同步变更
+    ) =>
 {
-    return Results.Json(setting);
+    return Results.Json(new
+    {
+        data1 = setting.Value,
+        data2 = setting2,
+        data3 = snapshot.Value
+    });
 });
 
 app.Run();
